e7ac11592ff97c8b24ec0a05c0cef7273fa95c7f
Benjamin <153460006+benjaminplayer@users.noreply.github.com>
2025-01-27 10:48:57 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -9,16 +9,17 @@
 const startButton = document.querySelector('.start');
 const resetButton = document.querySelector('.reset');
 resetButton.disabled = true;
+const speed = 4;
 startButton.addEventListener('click', () =>{
     let points = "234,2 234,10 170,10 170,26 202,26 202,42 186,42 186,90 170,90 170,74 154,74 154,90 138,90 138,74 122,74 122,58 106,58 106,42 58,42 58,58 90,58 90,106 122,106 122,122 106,122 106,138 138,138 138,186 122,186 122,234 106,234 106,250 122,250 122,266 90,266 90,298 74,298 74,266 58,266 58,282 42,282 42,266 10,266 10,282 26,282 26,298 58,298 58,314 42,314 42,330 26,330 26,314 10,314 10,362 26,362 26,346 42,346 42,362 58,362 58,330 74,330 74,394 90,394 90,362 106,362 106,378 122,378 122,394 154,394 154,426 138,426 138,442 170,442 170,458 186,458 186,426 170,426 170,394 186,394 186,410 202,410 202,426 314,426 314,442 298,442 298,458 314,458 314,474 282,474 282,458 266,458 266,474 250,474 250,482";
     let arr = create2DArrayFromCoordinates(points);
-    arr = extraPoints(arr);
-    drawSolution(arr);
+    arr = middlePoint(arr);
+    drawSolution(arr, speed);
 });
 
 resetButton.addEventListener('click', () =>{
     if(checkReset())
-        reset();
+        reset(speed);
 });
 
 function create2DArrayFromCoordinates(points) {
@@ -34,7 +35,7 @@ function create2DArrayFromCoordinates(points) {
     return result;
 }
 
-function drawSolution(points) {
+function drawSolution(points, speed) {
 
     //points="234,2 234,10 170,10 170,26 202,26 202,42 186,42 186,90 170,90 170,74 154,74 154,90 138,90 138,74 122,74 122,58 106,58 106,42 58,42 58,58 90,58 90,106 122,106 122,122 106,122 106,138 138,138 138,186 122,186 122,234 106,234 106,250 122,250 122,266 90,266 90,298 74,298 74,266 58,266 58,282 42,282 42,266 10,266 10,282 26,282 26,298 58,298 58,314 42,314 42,330 26,330 26,314 10,314 10,362 26,362 26,346 42,346 42,362 58,362 58,330 74,330 74,394 90,394 90,362 106,362 106,378 122,378 122,394 154,394 154,426 138,426 138,442 170,442 170,458 186,458 186,426 170,426 170,394 186,394 186,410 202,410 202,426 314,426 314,442 298,442 298,458 314,458 314,474 282,474 282,458 266,458 266,474 250,474 250,482"
     let polyPoints = "";
@@ -42,7 +43,6 @@ function drawSolution(points) {
     startButton.disabled = true;
     poly.setAttribute("points", "");
     poly.setAttribute('stroke','#ff0000');
-    console.log(points);
 
         let index = 0;
         let interval = setInterval(() => {
@@ -56,11 +56,12 @@ function drawSolution(points) {
             poly.setAttribute("points", polyPoints);
     
             index++;
-        }, 1);
+        }, speed);
 
 }
 
-function reset(){
+
+function reset(speed){
     let poly = document.querySelector("polyline");
     let polyPoints = poly.getAttribute('points');
     resetButton.disabled = true;
@@ -77,7 +78,7 @@ function reset(){
         polyPoints = polyPoints.substring(spaceIdx+1);
         poly.setAttribute('points', polyPoints);
 
-    },1)
+    },speed)
 }
 
 function checkReset(){
@@ -85,7 +86,7 @@ function checkReset(){
     return poly.getAttributeNames().includes("points");
 }
 
-//mazeToGrid();
+mazeToGrid();
 
 function mazeToGrid(){
     let svg = document.querySelector(".maze").children[0];
@@ -121,41 +122,49 @@ function mazeToGrid(){
         }
     }
 
+    addStartEnd();
+
+    function addStartEnd(){
+        let start = walls[0].getAttribute('y1');
+        let end = walls[walls.length-1].getAttribute('y2');
+        console.log("y1: "+start);
+        console.log("end: "+end)
+
+        let drawnStart = false
+
+        //narderi da 0 da v 2;
+
+        for(let i = 2; i < walls[0].length; i++){
+            if(walls[i] == 0){
+                
+            }
+        }
+
+    
+    }
     console.log(grid);
+
+
 }
 
-function extraPoints(points){
-    let extra ="";
-    // points[x][y]
+function middlePoint(points){
+    let extra = "";
     let start, end;
+    //points[x][y];
+
     for(let idx = 0; idx < points.length; idx++){
         if(idx === points.length-1)
             break;
-        
-        start = points[idx]; // dobi zacetni, ter koncni koordinati
-        end = points[idx+1];
-
-        if(start[0] == end[0]){ // ce sta x koordinati enaki -> vecaj y
-            if(start[1] < end[1]){ // ce je konec > zacetek
-                for (let i = parseInt(start[1]); i <= parseInt(end[1]); i++) {
-                    extra += start[0]+","+i+" ";    
-                }
-            }else{
-                for (let i = parseInt(start[1]); i >= parseInt(end[1]); i--) {
-                    extra += start[0]+","+i+" ";    
-                }
-            }
-    
-        }else{ // ce sta y koordinati enaki -> vecaj/mansaj x
-            if(start[0] < end[0])
-                for (let i = parseInt(start[0]); i <= parseInt(end[0]); i++)
-                    extra += i+","+start[1]+" ";    
-            else
-                for (let i = parseInt(start[0]); i >= parseInt(end[0]); i--)
-                    extra += i+","+start[1]+" ";    
+        let start = points[idx];
+        let end = points[idx+1];
+        if(Math.abs(start[0]) == Math.abs(end[0])){ // x1 = x2 -> y spremeni
+            extra += points[idx] + " " + start[0]+ "," + ((start[1]+end[1])/2)+" " + points[idx+1]+" ";
         }
-    }
+        else if(Math.abs(start[1]) == Math.abs(end[1])){
+            extra += points[idx] + " " + ((start[0]+end[0])/2) +","+ start[1]+" " + points[idx+1]+" ";
+        }
 
-    let points2 = create2DArrayFromCoordinates(extra);
-    return points2;
+    }
+   let points2 = create2DArrayFromCoordinates(extra);
+   return points2;
 }
